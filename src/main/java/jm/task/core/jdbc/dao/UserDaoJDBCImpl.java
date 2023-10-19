38c118a306ff6cdeb8d8d268ca7c238bb8927af6
Samadhi555 <9522224962@mail.ru>
2023-10-19 11:27:30 +0300

    Commit_3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -2,73 +2,87 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.sql.*;
-import java.util.List;
 import java.util.ArrayList;
-
+import java.util.List;
+import java.util.concurrent.Executor;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private final SessionFactory sessionFactory;
+    Connection connection;
 
     public UserDaoJDBCImpl() {
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
-        sessionFactory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+        connection = Util.getMySQLConnection();
     }
 
     public void createUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users (id BIGINT PRIMARY KEY, name VARCHAR(255), last_name VARCHAR(255), age TINYINT)").executeUpdate();
-            transaction.commit();
+        String createTable = "CREATE TABLE IF NOT EXISTS users " +
+                "(id INT AUTO_INCREMENT PRIMARY KEY, " +
+                " name VARCHAR(40), " +
+                " lastName VARCHAR(40), " +
+                " age TINYINT)";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(createTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void dropUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
-            transaction.commit();
+        String dropTable = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(dropTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            User user = new User(name, lastName, age);
-            session.save(user);
-            transaction.commit();
+        String insertUser = "INSERT INTO user (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insertUser)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public void removeUserById(long id) {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            User user = session.get(User.class, id);
-            if (user != null) {
-                session.delete(user);
-            }
-            transaction.commit();
+        String deleteUser = "DELETE FROM user WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(deleteUser)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
+
     }
 
     public List<User> getAllUsers() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM User", User.class).list();
+        List<User> userList = new ArrayList<>();
+        String selectAll = "SELECT * FROM user";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(selectAll)) {
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("lastName");
+                byte age = resultSet.getByte("age");
+                userList.add(new User(name, lastName, age));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return userList;
     }
 
     public void cleanUsersTable() {
-        try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createQuery("DELETE FROM User").executeUpdate();
-            transaction.commit();
+        String cleanTable = "DELETE FROM user";
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(cleanTable);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
